6e9a9de04893c1ace6c3e17d8dc6e45433c7356a
barantwenza <kazimabenny@.gmail.com>
2023-09-11 00:11:56 +0200

    API with models (without serializers)

--- bookApi/models.py
+++ bookApi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    number_of_pages = models.IntegerField()
+    published_date = models.DateField()
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
--- bookApi/views.py
+++ bookApi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
+from django.http import JsonResponse
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all() # Return data type known as Complex data
+    books_python = list(books.values()) # Python DS
+    # Converts in json
+    return JsonResponse({
+        'books': books_python
+    })
